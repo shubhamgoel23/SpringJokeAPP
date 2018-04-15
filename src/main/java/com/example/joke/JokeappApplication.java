d73c157f64aa2dfb322ee49e2c2c9aff1ab98a93
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-15 19:45:25 +0530

    spring xml based spring config

--- src/main/java/com/example/joke/JokeappApplication.java
+++ src/main/java/com/example/joke/JokeappApplication.java
@@ -1,9 +1,13 @@
 package com.example.joke;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
